dcf09ca374d3c72737bb3e3deb8567d575a6544f
Almaz <minnexanov.a@mail.ru>
2023-02-24 15:01:40 +0300

    seminar 9task 69

--- seminar9task69/Program.cs
+++ seminar9task69/Program.cs
@@ -7,7 +7,7 @@ Console.Write("Введите число: ");
 int userInput = int.Parse(Console.ReadLine()!)!;
 Console.Write("Введите степень: ");
 int userPow = int.Parse(Console.ReadLine()!)!;
-Console.WriteLine($"Сумма цифр числа равна {RecursePow(userInput, userPow)}");
+Console.WriteLine($"{RecursePow(userInput, userPow)}");
 
 int RecursePow(int inNum, int pow)
 {
