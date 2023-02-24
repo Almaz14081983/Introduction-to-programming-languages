d3969befaf1efe49610bbe191210422378866bd5
Almaz <minnexanov.a@mail.ru>
2023-02-24 13:02:33 +0300

    seminar 9task 66 dz

--- seminar9task66b/Program.cs
+++ seminar9task66b/Program.cs
@@ -5,6 +5,7 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+Console.Clear(); 
 Console.Write("Введите значение числа M: ");
 int meaningM = int.Parse(Console.ReadLine()!);
 
@@ -13,7 +14,6 @@ int meaningN = int.Parse(Console.ReadLine()!);
 
 int SumMN(int numberM, int numberN)
 {
-
     int res = numberM;
     if (numberM == numberN)
         return 0;
@@ -23,6 +23,5 @@ int SumMN(int numberM, int numberN)
         res = numberM + SumMN(numberM, numberN);
         return res;
     }
-
 }
 Console.WriteLine($"Cумма натуральных чисел в промежутке от M до N = {SumMN(meaningM-1, meaningN)}");
