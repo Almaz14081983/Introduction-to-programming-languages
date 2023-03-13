31b72104ca2582089ab9c53340fb3732cdaad047
Almaz <minnexanov.a@mail.ru>
2023-03-13 18:38:43 +0300

    sem New 2

---
+++ semNew2/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+
+string[] array1 = new string[5] {"13.", "03", "при", "вет", "пока"};
+string[] array2 = new string[array1.Length];
+
+
+int count = 0;
+for (int i = 0; i < array1.Length; i++)
+{
+if(array1[i].Length <= 3)
+    {
+    array2[count] = array1[i];
+    count++;
+    }
+}
+
+
+for (int i = 0; i < array2.Length; i++)
+{
+    Console.Write($"{array2[i]} ");
+}
+
+
